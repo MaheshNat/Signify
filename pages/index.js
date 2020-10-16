233b4cf17ebee1029c7985c777a3aa47a25d3fa8
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-10-16 13:49:19 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,187 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useRef, useState } from 'react';
+import cv from '../services/cv';
+import * as tf from '@tensorflow/tfjs';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
 
-export default function Home() {
+// We'll limit the processing size to 200px.
+const maxVideoSize = 224;
+const LETTERS = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  '_SPACE',
+  '_NOTHING',
+];
+
+/**
+ * What we're going to render is:
+ *
+ * 1. A video component so the user can see what's on the camera.
+ *
+ * 2. A button to generate an image of the video, load OpenCV and
+ * process the image.
+ *
+ * 3. A canvas to allow us to capture the image of the video and
+ * show it to the user.
+ */
+export default function Page() {
+  const videoElement = useRef(null);
+  const canvasEl = useRef(null);
+  const outputCanvasEl = useRef(null);
+  let model;
+  let [letter, setLetter] = useState(null);
+  let [loading, setLoading] = useState(true);
+  let [confidence, setConfidence] = useState(0);
+
+  /**
+   * In the onClick event we'll capture a frame within
+   * the video to pass it to our service.
+   */
+  async function processImage() {
+    if (
+      videoElement !== null &&
+      canvasEl !== null &&
+      typeof videoElement.current !== 'undefined' &&
+      videoElement.current !== null
+    ) {
+      const ctx = canvasEl.current.getContext('2d');
+      ctx.drawImage(videoElement.current, 0, 0, maxVideoSize, maxVideoSize);
+      const image = ctx.getImageData(0, 0, maxVideoSize, maxVideoSize);
+      // Processing image
+      const processedImage = await cv.imageProcessing(image);
+      // Render the processed image to the canvas
+      outputCanvasEl.current
+        .getContext('2d')
+        .putImageData(processedImage.data.payload, 0, 0);
+      const prediction = await model.predict(
+        tf.browser
+          .fromPixels(outputCanvasEl.current)
+          .sub(tf.tensor1d([103.939, 116.779, 123.68]))
+          .expandDims()
+      );
+      const max = await prediction.argMax(1).data();
+      setLetter(LETTERS[max]);
+      prediction.data().then((predictionData) => {
+        setConfidence(predictionData[max]);
+      });
+    }
+  }
+
+  /**
+   * In the useEffect hook we'll load the video
+   * element to show what's on camera.
+   */
+  useEffect(() => {
+    async function initCamera() {
+      videoElement.current.width = maxVideoSize;
+      videoElement.current.height = maxVideoSize;
+
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            facingMode: 'user',
+            width: maxVideoSize,
+            height: maxVideoSize,
+          },
+        });
+        videoElement.current.srcObject = stream;
+
+        return new Promise((resolve) => {
+          videoElement.current.onloadedmetadata = () => {
+            resolve(videoElement.current);
+          };
+        });
+      }
+      const errorMessage =
+        'This browser does not support video capture, or this device does not have a camera';
+      alert(errorMessage);
+      return Promise.reject(errorMessage);
+    }
+
+    async function load() {
+      const videoLoaded = await initCamera();
+      model = await tf.loadLayersModel(
+        'https://4977e0ca76d1.ngrok.io/modelv3.2raw_tfjs/model.json'
+      );
+      await cv.load();
+      videoLoaded.play();
+      setInterval(processImage, 0);
+      setLoading(false);
+      return videoLoaded;
+    }
+
+    load();
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+    <div className="container" style={{ marginTop: '2em' }}>
+      <div className="jumbotron text-center">
+        <h1>Signify - ASL Made Easy.</h1>
+        <p>
+          A simple sign language translator using machine learning to classify
+          signs by a convolutional neural network.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      </div>
+      {loading && <h1 className="text-center">Loading...</h1>}
+      <div style={{ display: loading ? 'none' : 'block' }}>
+        <div className="row justify-content-center">
+          <video className="video col-xs-12" playsInline ref={videoElement} />
+          <canvas
+            style={{ display: 'none' }}
+            ref={canvasEl}
+            width={maxVideoSize}
+            height={maxVideoSize}
+          ></canvas>
+          <canvas
+            className="col-xs-12"
+            ref={outputCanvasEl}
+            width={maxVideoSize}
+            height={maxVideoSize}
+          ></canvas>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className="row justify-content-center text-center"
+          style={{ marginTop: '2em' }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <div className="col-xs-12">
+            <h2>Predicted Letter:</h2>
+            <h1
+              style={{
+                borderRadius: 10,
+                border: '2px solid #FFFFFF',
+                padding: '1em',
+              }}
+            >
+              {letter}
+            </h1>
+            <h4>Confidence: {confidence}</h4>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
